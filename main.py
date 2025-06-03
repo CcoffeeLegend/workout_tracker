ad37e0bde1f33ed1b0242c559e03d1ef44f0b31c
Michael Amelian <mykovanov@gmail.com>
2025-06-03 04:16:18 -0400

    initial implementation of login and registration functions

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-print("How many sets would you like to do?")
-sets = input()
+def login_prompt() -> None:
+    
+    newuser = input("Hello! Are you a new or returning user?")
+    if newuser == "new":
+        user_registration()
+    else: 
+        run_menu()
 
-print("How many reps in each set?")
-reps = input()
+def user_registration() -> None
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
 
-print(f"So you'd like to do {sets} sets of {reps} reps?")
+# print("What exercise would you like to do?")
+# exercise = input()
+    
+# print("How many sets would you like to do?")
+# sets = input()
+
+# print("How many reps in each set?")
+# reps = input()
+
+# print(f"So you'd like to do {sets} sets of {reps} reps for {exercise}?")
