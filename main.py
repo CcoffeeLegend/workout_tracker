55a32f2f496f7e3311af4b2c5c64c211e895441f
Michael Amelian <mykovanov@gmail.com>
2025-06-06 04:25:29 -0400

    add_exercise expanded, login partially implemented

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ def save_users():
     with open("userdata.json", "w") as file:
         json.dump(userdata, file, indent=4)
 
+def user_login():
+    global userdata
+    input("Enter username: ").strip()
+    input("Enter password: ").strip()
+
+
 def login_prompt() -> None:
     #This function runs on startup, checks whether the user is new or not, then runs user_registration or  user_login respectively            
 
@@ -32,24 +38,31 @@ def user_registration() -> None:
     username = input("Enter a username: ").strip()
     password = input("Enter a password: ").strip()
 
-    create_routine()
+    userdata[username] = {
+        "password": password,
+        "routine": []
+    }
+
+    save_users()
+    create_routine(username)
 
 
-def create_routine() -> None:
+def create_routine(username: str) -> None:
     #This function runs for new users
-    add_exercise()
+    add_exercise(username)
 
 
-def add_exercise() -> None:
+def add_exercise(username: str) -> None:
     #This function adds exercises to the routine
     print("What exercise would you like to do?")
     exercise = input().strip()
         
-    print("How many sets would you like to do?")
-    sets = input().strip()
-
-    print("How many reps in each set?")
-    reps = input().strip()
+    sets = int(input("How many sets would you like to do? ").strip())
+    reps = int(input("How many reps in each set? ").strip())
+    weights = []
+    for i in range(1, sets + 1):
+        weight = int(input(f"Enter weight for set {i}: ").strip())
+        weights.append(weight)
 
     print(f"So you'd like to do {sets} sets of {reps} reps of {exercise}?")
     add_confirm = input("Confirm (yes/no): " ).strip()
@@ -58,8 +71,8 @@ def add_exercise() -> None:
         print(f"{exercise} added. Would you like to add another exercise?")
         add_another = input("Confirm (yes/no): ").strip()
         if add_another.lower() in ['yes', 'y']:
-            add_exercise()
+            add_exercise(username)
         else:
-            menu()
+            menu(username)
     else:
-        menu()
+        menu(username)
